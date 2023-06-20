9918644d697234112b393f61d9dea9ae497528f8
Florian Sipp <florian.sipp@chuv.ch>
2023-06-20 11:46:07 +0000

    fixup for ubuntu 22.04

--- get_ghostfs.sh
+++ get_ghostfs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-GHOSTFS_VERSION=898110c
+GHOSTFS_VERSION=1861209-dev
 
 rm GhostFS
-wget https://github.com/pouya-eghbali/ghostfs-builds/releases/download/linux-$GHOSTFS_VERSION/GhostFS
+wget https://github.com/pouya-eghbali/ghostfs-builds/releases/download/linux-ubuntu-22.04-$GHOSTFS_VERSION/GhostFS
 chmod +x GhostFS
 
 sudo pm2 restart pm2/ecosystem.server.config.js
