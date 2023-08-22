68a90068eb894e9dd7eb59c7ba3fdb3c9e3d9fe2
iDmple <iDmple@users.noreply.github.com>
2023-08-22 11:05:35 +0200

    Update get_ghostfs.sh

--- get_ghostfs.sh
+++ get_ghostfs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-GHOSTFS_VERSION=1861209-dev
+GHOSTFS_VERSION=1861209
 
 rm GhostFS
 wget https://github.com/pouya-eghbali/ghostfs-builds/releases/download/linux-ubuntu-22.04-$GHOSTFS_VERSION/GhostFS
